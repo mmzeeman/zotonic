115bbf18d2438627f5a70a168beff80a639f3792
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2013-10-16 10:37:35 +0200

    core: experimental flush of the mailbox before each request

--- include/controller_webmachine_helper.hrl
+++ include/controller_webmachine_helper.hrl
@@ -7,5 +7,13 @@ start_link(Args) ->
     webmachine_controller:start_link(?MODULE, [Args]).
 
 ping(ReqData, State) ->
+	%% Experimental flushing all messages from the mailbox.
+	flush_messages(),
     {pong, ReqData, State}.
 
+flush_messages() ->
+	receive 
+		_Msg -> flush_messages()
+	after 
+		0 -> ok
+	end.
